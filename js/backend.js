27328380967b491d67ef9d8009be23a90c29b260
unknown <guruprasath23062008@gmail.com>
2026-04-04 10:57:41 +0530

    bug fix

--- js/backend.js
+++ js/backend.js
@@ -5,7 +5,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 document.querySelector(".nextbtn")
   .addEventListener("click", click_next);
-
 async function click_next() {
 
   const voteid = document.getElementById("voteid").value;
@@ -25,15 +24,48 @@ async function click_next() {
       if (docSnap.exists()) {
 
 
+
+        window.addEventListener("offline", async () => {
+          console.log("Offline");
+          await updateDoc(docRef, {
+            access: false
+          });
+        });
+
+        window.addEventListener("online", async () => {
+          console.log("Back Online");
+
+          const access = docSnap.data().access;
+
+          if (access !== true) {
+            await updateDoc(docRef, {
+            access: true
+          });
+          }
+        });
+        window.addEventListener("beforeunload", async () => {
+          navigator.sendBeacon("/log"); // optional
+          await updateDoc(docRef, {
+            access: false
+          });
+        });
+        document.addEventListener('visibilitychange', () => {
+          if (document.visibilityState === 'hidden') {
+
+            navigator.sendBeacon('/api/exit-endpoint', updateDoc(docRef, {
+            access: false
+          }));
+          }
+        });
         if (ddate.getFullYear() == birth.getFullYear() && ddate.getMonth() == birth.getMonth() && ddate.getDate() == birth.getDate()) {
 
-          if (docSnap.data().isvoted === true ) {
+          if (docSnap.data().isvoted === true) {
             alert("you are already voted");
             document.getElementById("voteid").value = "";
             document.getElementById("birth").value = "";
             return;
           }
-          if(docSnap.data().access === true){
+          if (docSnap.data().access === true) {
             alert("You have already accessed the voting page. Please proceed to vote.");
             document.getElementById("voteid").value = "";
             return
@@ -45,17 +77,17 @@ async function click_next() {
           document.getElementById("voteid").value = "";
           document.getElementById("birth").value = "";
           await updateDoc(docRef, {
-            access:true
-        });
+            access: true
+          });
           window.location.href = "aadhar.html";
 
         } else {
           alert("Invalid Login Details");
         }
       }
-    }else {
-          alert("Invalid Login Details");
-        }
+    } else {
+      alert("Invalid Login Details");
+    }
   } catch (error) {
     alert("Record not found");
   }
--- js/facelock.js
+++ js/facelock.js
@@ -26,11 +26,11 @@ let savedDescriptor = null;
 
 // Load Models
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.ageGenderNet.loadFromUri('/models')
-]).then(fetchFaceFromDatabase);
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.ageGenderNet.loadFromUri('./models') 
+]).then(startVideo);
 
 async function fetchFaceFromDatabase() {
   statusText.innerText = "Connecting to Database...";
